4d7199548fa44dbcff9b3698de402393d7864f1f
Jason Seminara <js@ga.co>
2018-06-14 19:02:22 -0400

    act 2 done

--- act2.sh.js
+++ act2.sh.js
@@ -4,10 +4,21 @@ echo "Help me, Obi-Wan…You’re my only hope." > star_wars/rebellion/princess_
 # 2. Create a file called obi_wan in star_wars/rebellion.
 touch star_wars/rebellion/obi_wan
 
-# 3. Create a file in star_wars/rebellion called luke_skywalker.
+# 3. Create a *file* in star_wars/rebellion called luke_skywalker.
 touch star_wars/rebellion/luke_skywalker
 
-# 5. Create a directory in star_wars/rebellion called millenium_falcon.
-# 6. Inside the millenium_falcon, create two files: han_solo and chewy.
-# 7. Move luke_skywalker, obi_wan, and princess_leia into the millenium_falcon.
-# 8. Move the millenium_falcon into the death_star.
+# 4. Create a *directory* in star_wars/rebellion called millenium_falcon.
+mkdir star_wars/rebellion/millenium_falcon
+
+# 5. Inside the millenium_falcon, create two files: han_solo and chewy.
+touch star_wars/rebellion/millenium_falcon/han_solo
+touch star_wars/rebellion/millenium_falcon/chewy
+
+# 6. In THREE commands, Move luke_skywalker, obi_wan, and princess_leia into the millenium_falcon, respectively.
+mv star_wars/rebellion/luke_skywalker star_wars/rebellion/millenium_falcon/
+mv star_wars/rebellion/obi_wan star_wars/rebellion/millenium_falcon/
+mv star_wars/rebellion/princess_leia star_wars/rebellion/millenium_falcon/
+
+
+# 7. Move the millenium_falcon into the death_star.
+mv star_wars/rebellion/millenium_falcon star_wars/empire/death_star/
--- test/act2.test.js
+++ test/act2.test.js
@@ -7,8 +7,9 @@ const readShellCommands = require('../lib/readShellCommands');
 const setUp = (done) => {
   const act1 = readShellCommands('act1.sh.js').join(';');
   exec(act1, done);
-}
-//const tearDown = done => exec('rm -rf star_wars', done);
+};
+
+const tearDown = done => exec('rm -rf star_wars', done);
 
 const commands = readShellCommands('act2.sh.js');
 const tests = [
@@ -30,21 +31,57 @@ const tests = [
     },
   },
   {
-
     'should create a file called star_wars/rebellion/luke_skywalker': () => {
       assert.isTrue(fs.existsSync('star_wars/rebellion/luke_skywalker'), 'star_wars/rebellion/luke_skywalker should exist');
     },
   },
+  {
+    'should create a directory called star_wars/rebellion/millenium_falcon': () => {
+      assert.isTrue(fs.existsSync('star_wars/rebellion/millenium_falcon'), 'star_wars/rebellion/millenium_falcon should exist');
+    },
+  },
+  {
+    'should create a file called star_wars/rebellion/millenium_falcon/han_solo': () => {
+      assert.isTrue(fs.existsSync('star_wars/rebellion/millenium_falcon/han_solo'), 'star_wars/rebellion/millenium_falcon/han_solo should exist');
+    },
+  },
+  {
+    'should create a file called star_wars/rebellion/millenium_falcon/chewy': () => {
+      assert.isTrue(fs.existsSync('star_wars/rebellion/millenium_falcon/chewy'), 'star_wars/rebellion/millenium_falcon/chewy should exist');
+    },
+  },
+  {
+    'should move luke_skywalker into star_wars/rebellion/millenium_falcon': () => {
+      assert.isFalse(fs.existsSync('star_wars/rebellion/luke_skywalker'), 'star_wars/rebellion/luke_skywalker should NOT exist');
+      assert.isTrue(fs.existsSync('star_wars/rebellion/millenium_falcon/luke_skywalker'), 'star_wars/rebellion/millenium_falcon/luke_skywalker should exist');
+    },
+  },
+  {
+    'should move obi_wan into star_wars/rebellion/millenium_falcon': () => {
+      assert.isFalse(fs.existsSync('star_wars/rebellion/obi_wan'), 'star_wars/rebellion/obi_wan should NOT exist');
+      assert.isTrue(fs.existsSync('star_wars/rebellion/millenium_falcon/obi_wan'), 'star_wars/rebellion/millenium_falcon/obi_wan should exist');
+    },
+  },
+  {
+    'should move princess_leia into star_wars/rebellion/millenium_falcon': () => {
+      assert.isFalse(fs.existsSync('star_wars/rebellion/princess_leia'), 'star_wars/rebellion/princess_leia should NOT exist');
+      assert.isTrue(fs.existsSync('star_wars/rebellion/millenium_falcon/princess_leia'), 'star_wars/rebellion/millenium_falcon/princess_leia should exist');
+    },
+  },
+  {
+    'should move millenium_falcon into star_wars/empire/death_star': () => {
+      assert.isFalse(fs.existsSync('star_wars/rebellion/millenium_falcon'), 'star_wars/rebellion/millenium_falcon should NOT exist');
+      assert.isTrue(fs.existsSync('star_wars/empire/death_star/millenium_falcon'), 'star_wars/empire/death_star/millenium_falcon should exist');
+    },
+  },
 ];
 
-console.log(tests, commands)
 describe('Act 2', () => {
   // set up
   before(setUp);
-
+  after(tearDown);
   // dynamically generate the tests based on each of the commands
   tests.forEach((test, i) => {
-    console.log(i)
     // our test is an array of arrays.
     // use destructuring to separate the key from the val
     const [[should, f]] = Object.entries(test);
